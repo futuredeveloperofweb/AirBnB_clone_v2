9c49b4cf9c9a167d703cec0c831d8efe5731b5d5
othmane <othmane.alaouimekkaoui@gmail.com>
2024-01-17 14:23:17 -0800

    update

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+'''Module to test the class City'''
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
@@ -22,3 +22,28 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+class TestCity(unittest.TestCase):
+    '''test the class City'''
+    @classmethod
+    def teardown(cls):
+        '''delete the contents of the class when deleted'''
+        del cls.city
+
+    def test_is_subclass(self):
+        '''check if City is subsclass of BaseModel'''
+        self.assertTrue(issubclass(self.city.__class__, BaseModel), True)
+
+    def test_attributes_City(self):
+        '''check the existance of class attributes'''
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+
+    def test_attribute_types(self):
+        '''check the type of the attrs'''
+        self.assertEqual(type(self.city.state_id), str)
+        self.assertEqual(type(self.city.name), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
