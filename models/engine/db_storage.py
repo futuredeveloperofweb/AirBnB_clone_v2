b0833cfb913e30a2b34acdb853eca7e4453d4cb7
fao-en <fennaama@outlook.com>
2024-01-18 07:55:48 +0100

    db_storage fix error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,7 +73,7 @@ class DBStorage:
         """delete an element in the table
         """
         if obj:
-            self.session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         """configuration
