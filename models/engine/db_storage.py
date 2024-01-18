d4f3903936e37c40179ca9f9d1149f65737143e3
fao-en <fennaama@outlook.com>
2024-01-18 15:18:04 +0100

    update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,14 +2,17 @@
 """Defines the DBStorage engine."""
 from os import getenv
 from models.base_model import Base
-from models.state import State
+from models.base_model import BaseModel
+from models.amenity import Amenity
 from models.city import City
-from models.user import User
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
+from models.state import State
+from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
