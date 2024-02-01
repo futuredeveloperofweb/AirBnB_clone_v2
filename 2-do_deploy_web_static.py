eb01cafb9de1d549a3c6e4ff991dba9758a9710f
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-01 11:51:54 -0800

    update t2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["44.210.150.159", "35.173.47.15"]
+env.hosts = ["54.237.42.80", "54.221.185.211"]
 env.user = "ubuntu"
 
 
