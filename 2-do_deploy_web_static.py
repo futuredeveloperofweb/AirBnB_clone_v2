14a9c7e99428bd321deee24a49250c20ae04ff3c
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-01 11:12:49 -0800

    t2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,26 +24,22 @@ def do_deploy(archive_path):
     if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed:
         return False
 
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, name)).failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name)).failed:
         return False
 
     if run("rm /tmp/{}".format(file_name)).failed:
         return False
 
-    if run("mv / data/web_static/releases/{}/web_static/*
-            / data/web_static/releases/{} /".format(name, name)).failed:
+    if run("mv / data/web_static/releases/{}/web_static/* / data/web_static/releases/{} /".format(name, name)).failed:
         return False
 
-    if run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(name)).failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed:
         return False
 
     if run("rm -rf /data/web_static/current").failed:
         return False
 
-    if run("ln - s / data/web_static/releases/{} /
-            /data/web_static/current".format(name)).failed:
+    if run("ln - s / data/web_static/releases/{} / /data/web_static/current".format(name)).failed:
         return False
 
     return True
