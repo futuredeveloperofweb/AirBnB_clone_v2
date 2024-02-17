df4109db3edf695cab426979a12a12a4a8a726a0
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-17 13:32:23 -0800

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''script that starts a Flask web application'''
+'''script that starts a Flask web application:'''
 from flask import Flask
 
 
@@ -23,6 +23,16 @@ def text(text):
     '''display “C ” followed by the value of the text variable
     (replace underscore _ symbols with a space )'''
     return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text2(text):
+    '''display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )'''
+    if text is None:
+        return 'Python is cool'
+    else:
+        return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
