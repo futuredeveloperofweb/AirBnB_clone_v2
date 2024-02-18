ab8000a1e09bcd451dfab8d362c37b9b5a238d34
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-17 16:19:23 -0800

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+from models import storage
+from flask import render_template
+from models import *
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state():
+    '''display a HTML page: (inside the tag BODY)'''
+    states = sorted(list(storage.all("State").values()),
+                    key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    '''Call in this method storage.close()'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
