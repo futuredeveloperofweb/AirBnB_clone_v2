de4d7fb8e8e3155a0d5a09360d0b268e196b16e0
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-19 13:14:45 -0800

    up

--- models/state.py
+++ models/state.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
+import models
+import shlex
+from os import getenv
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class / table model"""
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities_relation = relationship("City", backref="state",
+                                   cascade="all, delete, delete-orphan")
 
-        @property
-        def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    @property
+    def cities(self):
+        """Get a list of all related City objects."""
+        var = models.storage.all()
+        city_list = []
+        result = []
+
+        for key in var:
+            city_key = key.replace('.', ' ')
+            city_key = shlex.split(city_key)
+            if city_key[0] == 'City':
+                city_list.append(var[key])
+
+        for city in city_list:
+            if city.state_id == self.id:
+                result.append(city)
+
+        return result
